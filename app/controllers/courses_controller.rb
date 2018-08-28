22d309d8f230c416ba9b0f53f721310e00e36cf6
frcharry <fabio.charry@gmail.com>
2018-08-27 21:51:32 -0500

    Segundo commit finaliza CRUD

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
+# El controlador lo utilzamos para definir las operaciones CRUD, la vista general "Index", la Vista particular "Show".
 class CoursesController < ApplicationController
   def index #Muestra todos los registros // GET
+    @courses = Course.all #asigna todos los cursos que han sido creados
   end
   def new #Devuelve una forma a través de la cual podemos crear el objeto // GET
     @course = Course.new #Variable con '@' de alcance global, se puede llamar desde la vista.
@@ -15,12 +17,26 @@ class CoursesController < ApplicationController
     end
   end
   def show #Muestra un objeto // GET
+    @course=Course.find(params[:id]) #indicamos que queremos encontrar el curso con el id que estamos enviando en nuestros parámetros
   end
   def edit #Devuelve una forma a través de la cual podemos modificar el objeto // GET
+    @course=Course.find(params[:id])
   end
   def update # Modofica el objeto // PATCH, PUT
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      flash[:notice] = "Se ha actualizado el curso."
+      redirect_to @course
+    else
+      flash[:error]="No se ha actualizado el curso"
+      render action: "edit" #vuelve a mostrar la forma
+    end
   end
   def destroy # Elimina el objeto // DELETE
+    @course=Course.find(params[:id])
+    @course.destroy
+    flash[:notice]="El curso se ha eliminado."
+    redirect_to courses_path
   end
   private
     def course_params #los únicos parámetros que se van a guardar son el nombre y la descripción, al restringir los datos se cierra la vulnerabilidad de exponer todos los parámetros del objeto.
---
+++ spec/features/editing_courses_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Usarios pueden editar cursos" do
+  scenario "con atributos válidos" do
+    course = Course.create(name:"Matemáticas",description:"Es un curso de matemáticas básicas")
+    visit "/"
+    click_link "Matemáticas"
+    click_link "Editar"
+    fill_in "Name",with: "Matemáticas Avanzadas"
+    click_button "Guardar"
+    expect(page).to have_content "Se ha actualizado el curso."
+    expect(page).to have_content "Matemáticas Avanzadas"
+  end
+end
